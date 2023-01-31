8a3a6f5625cbde8a4bd0ff31dc2dc578f8c6d246
Slava <mistik99@mail.ru>
2023-01-31 21:01:47 +0200

    Задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите числа через пробел: ");
+int count = 0;
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+        count++;
+}
+Console.WriteLine($"Кол-во элементов > 0: {count}");
